d529504c13ca2b7f954bcda9c37fbec423da6184
murodbeksaburov163-coder <murodbek.saburov163@gmail.com>
2026-07-30 02:21:51 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # Bot tokeningizni shu yerga yozing yoki muhit o'zgaruvchisi orqali bering:
 #   export BOT_TOKEN="123456:ABC-DEF..."
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8619518195:AAEfJQwnEhnc5V5Ohw2SM7CMcv0V04jYiL4")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8619518195:AAFBKagDpFWgTPfhXDAt5yHwsm2M2H31dDE")
 
 # Majburiy obuna talab qilinadigan kanal va chat (username, @ belgisisiz saqlaymiz)
 REQUIRED_CHANNEL = "@K0NKURS_UZ"
